e097e659247b28e985b4ad315860d3f3fb5f5e8e
thorcc <thorchrcow@gmail.com>
2021-01-25 15:31:34 +0100

    startkode

---
+++ objekt.js
@@ -0,0 +1,27 @@
+let trommer = {
+    a: {
+        lyd: "hihat.wav",
+        navn: "Hi-hat",
+        aktiv: false
+    },
+    s: {
+        lyd: "bass.wav",
+        navn: "Basstromme",
+        aktiv: false
+    },
+    d: {
+        lyd: "skarp.wav",
+        navn: "Skarptromme",
+        aktiv: false
+    },
+    f: {
+        lyd: "tom.wav",
+        navn: "Tom-tom",
+        aktiv: false
+    },
+    g: {
+        lyd: "cymbal.wav",
+        navn: "Cymbal",
+        aktiv: false
+    }
+};
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,4 +5,23 @@
 	h1{
 		text-align: center;
 	}
+	.trommer{
+		position: absolute;
+		display: flex;
+		gap: 20px;
+		justify-content: center;
+		align-items: center;
+		height: 80vh;
+		width: 99vw;
+	}
+	.tromme{
+		width: 120px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		border: 3px solid black;
+	}
+	.aktiv{
+		border-color: darkgoldenrod;
+	}
 </style>
